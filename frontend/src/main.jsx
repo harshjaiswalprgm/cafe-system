beaf0a56f13a9027ae77dcc86e386c5ca22d3b90
Harsh Jaiswal <harshjaiswal.prgm@gmail.com>
2026-01-10 02:25:16 +0530

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/cafe-system">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
